a6832ce0b49bdb3955271aa21268098b556cb2cb
SiddharthAnsh <74598989+SiddharthAnsh@users.noreply.github.com>
2025-02-18 16:21:51 +0530

    Initial Commit

---
+++ Project3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project3
+//
+//  Created by Siddharth Ansh on 18/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project3Tests/Project3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project3Tests.swift
+//  Project3Tests
+//
+//  Created by Siddharth Ansh on 18/02/25.
+//
+
+import Testing
+@testable import Project3
+
+struct Project3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
